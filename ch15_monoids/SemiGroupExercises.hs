09191261da97cd231a6160d19d41af131e873872
Joel Wilson <JoelWWilson@gmail.com>
2016-11-13 17:07:38 -0800

    Do about 1/2 monoid exercises

--- ch15_monoids/SemiGroupExercises.hs
+++ ch15_monoids/SemiGroupExercises.hs
@@ -6,15 +6,26 @@ import Data.Semigroup as S
 ---------------------------------
 -- Use QC to validate all instances (just associativity).
 
--- Need this for testing associativity law of Semigroup
+-- Need this for testing associativity law
 semigroupAssoc :: (Eq a, Semigroup a) => a -> a -> a -> Bool
 semigroupAssoc a b c = (a <> (b <> c)) == ((a <> b) <> c)
+
+-- For testing identity law of monoids
+monoidLeftIdentity :: (Eq m, Monoid m) => m -> Bool
+monoidLeftIdentity a = (mempty `mappend` a) == a
+
+monoidRightIdentity :: (Eq m, Monoid m) => m -> Bool
+monoidRightIdentity a = (a `mappend` mempty) == a
 
 -- 1. Trivial
 data Trivial = Trivial deriving (Eq, Show)
 
 instance Semigroup Trivial where
   x <> x' = Trivial
+
+instance Monoid Trivial where
+  mempty = Trivial
+  mappend = (<>)
 
 instance Arbitrary Trivial where
   arbitrary = return Trivial
@@ -27,6 +38,12 @@ newtype Identity a = Identity a deriving (Eq, Show)
 instance Semigroup a => Semigroup (Identity a) where
   (Identity a) <> (Identity a') = Identity (a <> a')
 
+instance (Num a, Semigroup a) => Monoid (Identity a) where
+  -- Ironically, there is no identity (mempty) unless we add
+  -- a type constraint, such as `Num a` as I've done here.
+  mempty = Identity 0
+  mappend = (<>)
+
 -- Generate arbitrary values of Identity a.
 identityGen :: Arbitrary a => Gen (Identity a)
 identityGen = do
@@ -36,6 +53,12 @@ identityGen = do
 instance Arbitrary a => Arbitrary (Identity a) where
   arbitrary = identityGen
 
+-- This is needed to use Sum in main...maybe this isn't right.
+instance Arbitrary a => Arbitrary (Sum a) where
+  arbitrary = do
+    a <- arbitrary
+    return $ Sum a
+
 type IdentityAssoc = Identity String -> Identity String -> Identity String -> Bool
 
 -- 3. Two a b
@@ -44,6 +67,10 @@ data Two a b = Two a b
 
 instance (Semigroup a, Semigroup b) => Semigroup (Two a b) where
   (Two a b) <> (Two a' b') = Two (a <> a') (b <> b')
+
+instance (Num a, Num b, Semigroup a, Semigroup b) => Monoid (Two a b) where
+  mempty = Two 0 0
+  mappend = (<>)
 
 twoGen :: (Arbitrary a, Arbitrary b) => Gen (Two a b)
 twoGen = do
@@ -68,6 +95,11 @@ instance (Semigroup a, Semigroup b, Semigroup c) =>
   Semigroup (Three a b c) where
   (Three a b c) <> (Three a' b' c') = Three (a <> a') (b <> b') (c <> c')
 
+instance (Num a, Num b, Num c, Semigroup a, Semigroup b, Semigroup c) =>
+  Monoid (Three a b c) where
+    mempty = Three 0 0 0
+    mappend = (<>)
+
 threeGen :: (Arbitrary a, Arbitrary b, Arbitrary c) => Gen (Three a b c)
 threeGen = do
   a <- arbitrary
@@ -84,6 +116,8 @@ type ThreeAssoc =
   -> Three String String String
   -> Bool
 
+type ThreeId = Three (Sum Integer) (Sum Integer) (Sum Integer) -> Bool
+
 -- 5. data Four a b c d
 data Four a b c d = Four a b c d
   deriving (Eq, Show)
@@ -91,7 +125,13 @@ data Four a b c d = Four a b c d
 -- Semigroup instance
 instance (Semigroup a, Semigroup b, Semigroup c, Semigroup d) =>
   Semigroup (Four a b c d) where
-  (Four a b c d) <> (Four a' b' c' d') = Four (a <> a') (b <> b') (c <> c') (d <> d')
+    (Four a b c d) <> (Four a' b' c' d') =
+      Four (a <> a') (b <> b') (c <> c') (d <> d')
+
+instance (Num a, Num b, Num c, Num d, Semigroup a, Semigroup b, Semigroup c, Semigroup d) =>
+  Monoid (Four a b c d) where
+    mempty = Four 0 0 0 0
+    mappend = (<>)
 
 -- Gen function to use as the arbitrary function
 fourGen :: (Arbitrary a, Arbitrary b, Arbitrary c, Arbitrary d) => Gen (Four a b c d)
@@ -114,14 +154,21 @@ type FourAssoc =
   -> Four String String String String
   -> Bool
 
+type FourId = Three (Sum Integer) (Sum Integer) (Sum Integer) -> Bool
+
 -- 6. newtype BoolConj
 newtype BoolConj = BoolConj Bool
   deriving (Eq, Show)
 
 -- semigroup instance
 instance Semigroup BoolConj where
-  (BoolConj True) <> (BoolConj True)  = BoolConj True
+  (BoolConj True) <> (BoolConj False) = BoolConj True
+  (BoolConj False) <> (BoolConj True) = BoolConj True
   _ <> _ = BoolConj False
+
+instance Monoid BoolConj where
+  mempty = BoolConj False
+  mappend = (<>)
 
 -- gen func
 boolConjGen :: Gen BoolConj
@@ -144,6 +191,10 @@ instance Semigroup BoolDisj where
   (BoolDisj False) <> (BoolDisj False) = BoolDisj False
   _ <> _ = BoolDisj True
 
+instance Monoid BoolDisj where
+  mempty = BoolDisj False
+  mappend = (<>)
+
 boolDisjGen :: Gen BoolDisj
 boolDisjGen = oneof [
   return $ BoolDisj True,
@@ -161,9 +212,13 @@ data Or a b =
   deriving (Eq, Show)
 
 instance Semigroup (Or a b) where
-  (Fst a) <> (Fst b) = Fst b
-  (Fst a) <> (Snd b) = Snd b
+  (Fst _) <> (Fst b) = Fst b
+  (Fst _) <> (Snd b) = Snd b
   (Snd a) <> _       = Snd a
+
+instance Num a => Monoid (Or a b) where
+  mempty = Fst 0
+  mappend = (<>)
 
 orGen :: (Arbitrary a, Arbitrary b) => Gen (Or a b)
 orGen = do
@@ -302,14 +357,44 @@ type AccumBothAssoc =
 
 main :: IO ()
 main = do
+  putStrLn "Testing Trivial..."
   quickCheck (semigroupAssoc :: TrivialAssoc)
+  quickCheck (monoidLeftIdentity :: Trivial -> Bool)
+  quickCheck (monoidRightIdentity :: Trivial -> Bool)
+
+  putStrLn "\nTesting Identity..."
   quickCheck (semigroupAssoc :: IdentityAssoc)
+  quickCheck (monoidLeftIdentity :: Sum Integer -> Bool)
+  quickCheck (monoidRightIdentity :: Sum Integer -> Bool)
+
+  putStrLn "\nTesting Two..."
   quickCheck (semigroupAssoc :: TwoAssoc)
+  quickCheck (monoidLeftIdentity :: Two (Sum Integer) (Sum Integer) -> Bool)
+  quickCheck (monoidRightIdentity :: Two (Sum Integer) (Sum Integer) -> Bool)
+
+  putStrLn "\nTesting Three..."
   quickCheck (semigroupAssoc :: ThreeAssoc)
+  quickCheck (monoidLeftIdentity :: ThreeId)
+  quickCheck (monoidRightIdentity :: ThreeId)
+
+  putStrLn "\nTesting Four..."
   quickCheck (semigroupAssoc :: FourAssoc)
+  quickCheck (monoidLeftIdentity :: FourId)
+  quickCheck (monoidRightIdentity :: FourId)
+
+  putStrLn "\nTesting BoolConj..."
   quickCheck (semigroupAssoc :: BoolConjAssoc)
+  quickCheck (monoidLeftIdentity :: BoolConj -> Bool)
+  quickCheck (monoidRightIdentity :: BoolConj -> Bool)
+
+  putStrLn "\nTesting BoolDisj..."
   quickCheck (semigroupAssoc :: BoolDisjAssoc)
+  quickCheck (monoidLeftIdentity :: BoolDisj -> Bool)
+  quickCheck (monoidRightIdentity :: BoolDisj -> Bool)
+
+  putStrLn "\nTesting Or..."
   quickCheck (semigroupAssoc :: OrAssoc)
+
   quickCheck (semigroupAssoc :: ValidationAssoc)
   quickCheck (semigroupAssoc :: AccumRightAssoc)
   quickCheck (semigroupAssoc :: AccumBothAssoc)
